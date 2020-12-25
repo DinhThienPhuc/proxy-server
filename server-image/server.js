497d598d6754ef658aeccb92383ddb3899b1c4e1
Đinh Thiên Phúc <thienphuc.bka1412@gmail.com>
2020-12-25 17:34:01 +0700

    Add: server-image truely

--- server-image/server.js
+++ server-image/server.js
@@ -3,47 +3,15 @@ const cors = require('cors')
 const morgan = require('morgan')
 
 const app = express()
-const port = 8000
+const port = 8002
 
 app.set('trust proxy', true)
 
 app.use(cors())
 app.use(morgan('combined'))
 
-app.get('/cache/:path', (req, res) => {
-  const date = new Date()
-
-  res.json({
-    message: 'Cache route',
-    route: `/${req.params.path}`,
-    data: {
-      time: date.toLocaleString(),
-    },
-  })
-})
-
-app.get('/:path', (req, res) => {
-  const date = new Date()
-
-  res.json({
-    message: 'All route',
-    route: `/${req.params.path}`,
-    data: {
-      time: date.toLocaleString(),
-    },
-  })
-})
-
 app.get('/', (_, res) => {
-  const date = new Date()
-
-  res.json({
-    message: 'Homepage',
-    route: '/',
-    data: {
-      time: date.toLocaleString(),
-    },
-  })
+  res.sendFile(__dirname + '/joke.jpg')
 })
 
 app.listen(port, () => console.log('Server is running'))
