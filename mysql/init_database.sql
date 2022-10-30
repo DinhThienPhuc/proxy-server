f247de4841fc7ee52382a631799d6056d462a858
DinhThienPhuc <thienphuc.bka1412@gmail.com>
2022-10-31 03:21:34 +0700

    stash

--- mysql/init_database.sql
+++ mysql/init_database.sql
@@ -81,14 +81,14 @@ INSERT INTO `exams_questions` (`id`, `exam_id`, `question_id`) VALUES (9, 1, 9);
 INSERT INTO `exams_questions` (`id`, `exam_id`, `question_id`) VALUES (10, 1, 10);
 INSERT INTO `exams_questions` (`id`, `exam_id`, `question_id`) VALUES (11, 2, 1);
 INSERT INTO `exams_questions` (`id`, `exam_id`, `question_id`) VALUES (12, 2, 3);
-INSERT INTO `exams_questions` (`id`, `exam_id`, `question_id`) VALUES (13, 2, 5);
+INSERT INTO `exams_questions` (`id`, `exam_id`, `question_id`) VALUES (13, 2, 4);
 INSERT INTO `exams_questions` (`id`, `exam_id`, `question_id`) VALUES (14, 2, 7);
-INSERT INTO `exams_questions` (`id`, `exam_id`, `question_id`) VALUES (15, 2, 9);
-INSERT INTO `exams_questions` (`id`, `exam_id`, `question_id`) VALUES (16, 2, 11);
+INSERT INTO `exams_questions` (`id`, `exam_id`, `question_id`) VALUES (15, 2, 8);
+INSERT INTO `exams_questions` (`id`, `exam_id`, `question_id`) VALUES (16, 2, 12);
 INSERT INTO `exams_questions` (`id`, `exam_id`, `question_id`) VALUES (17, 2, 13);
-INSERT INTO `exams_questions` (`id`, `exam_id`, `question_id`) VALUES (18, 2, 15);
-INSERT INTO `exams_questions` (`id`, `exam_id`, `question_id`) VALUES (19, 2, 17);
-INSERT INTO `exams_questions` (`id`, `exam_id`, `question_id`) VALUES (20, 2, 19);
+INSERT INTO `exams_questions` (`id`, `exam_id`, `question_id`) VALUES (18, 2, 14);
+INSERT INTO `exams_questions` (`id`, `exam_id`, `question_id`) VALUES (19, 2, 20);
+INSERT INTO `exams_questions` (`id`, `exam_id`, `question_id`) VALUES (20, 2, 21);
 COMMIT;
 
 -- ----------------------------
@@ -158,59 +158,39 @@ INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `description`, `g
 COMMIT;
 
 -- ----------------------------
--- Table structure for users_exams
+-- Table structure for users_exams_questions
 -- ----------------------------
-DROP TABLE IF EXISTS `users_exams`;
-CREATE TABLE `users_exams` (
+DROP TABLE IF EXISTS `users_exams_questions`;
+CREATE TABLE `users_exams_questions` (
   `id` int NOT NULL AUTO_INCREMENT,
   `user_id` int DEFAULT NULL,
   `exam_id` int DEFAULT NULL,
-  `score` text DEFAULT NULL,
+  `question_id` int DEFAULT NULL,
+  `user_answer` text,
+  `exam_score` text,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   KEY `exam_id` (`exam_id`),
-  CONSTRAINT `users_exams_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
-  CONSTRAINT `users_exams_ibfk_2` FOREIGN KEY (`exam_id`) REFERENCES `exams` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
-
--- ----------------------------
--- Records of users_exams
--- ----------------------------
-BEGIN;
-INSERT INTO `users_exams` (`id`, `user_id`, `exam_id`, `score`) VALUES (1, 123456, 1, '60/100');
-INSERT INTO `users_exams` (`id`, `user_id`, `exam_id`, `score`) VALUES (2, 123456, 2, '85/100');
-INSERT INTO `users_exams` (`id`, `user_id`, `exam_id`, `score`) VALUES (3, 123456, 3, '60/100');
-INSERT INTO `users_exams` (`id`, `user_id`, `exam_id`, `score`) VALUES (4, 123456, 4, NULL);
-INSERT INTO `users_exams` (`id`, `user_id`, `exam_id`, `score`) VALUES (5, 123456, 5, NULL);
-COMMIT;
-
--- ----------------------------
--- Table structure for users_questions
--- ----------------------------
-DROP TABLE IF EXISTS `users_questions`;
-CREATE TABLE `users_questions` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `user_id` int DEFAULT NULL,
-  `question_id` int DEFAULT NULL,
-  `user_answer` text,
-  PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`),
   KEY `question_id` (`question_id`),
-  CONSTRAINT `users_questions_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
-  CONSTRAINT `users_questions_ibfk_2` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`)
+  CONSTRAINT `users_exams_questions_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `users_exams_questions_ibfk_2` FOREIGN KEY (`exam_id`) REFERENCES `exams` (`id`),
+  CONSTRAINT `users_exams_questions_ibfk_3` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
 
 -- ----------------------------
--- Records of users_exams
+-- Records of users_exams_questions
 -- ----------------------------
 BEGIN;
-INSERT INTO `users_questions` (`id`, `user_id`, `question_id`, `user_answer`) VALUES (1, 123456, 1, "2::3");
-INSERT INTO `users_questions` (`id`, `user_id`, `question_id`, `user_answer`) VALUES (2, 123456, 2, "1");
-INSERT INTO `users_questions` (`id`, `user_id`, `question_id`, `user_answer`) VALUES (3, 123456, 3, "up");
-INSERT INTO `users_questions` (`id`, `user_id`, `question_id`, `user_answer`) VALUES (4, 123456, 4, "4::2");
-INSERT INTO `users_questions` (`id`, `user_id`, `question_id`, `user_answer`) VALUES (5, 123456, 5, "2");
-INSERT INTO `users_questions` (`id`, `user_id`, `question_id`, `user_answer`) VALUES (6, 123456, 6, "with");
-INSERT INTO `users_questions` (`id`, `user_id`, `question_id`, `user_answer`) VALUES (7, 123456, 7, "4::3::2");
+INSERT INTO `users_exams_questions` (`id`, `user_id`, `exam_id`, `question_id`, `user_answer`, `exam_score`) VALUES (1, 123456, 1, 1, "2::3", "80/100");
+INSERT INTO `users_exams_questions` (`id`, `user_id`, `exam_id`, `question_id`, `user_answer`, `exam_score`) VALUES (2, 123456, 1, 2, "1", "80/100");
+INSERT INTO `users_exams_questions` (`id`, `user_id`, `exam_id`, `question_id`, `user_answer`, `exam_score`) VALUES (3, 123456, 1, 3, "up", "80/100");
+INSERT INTO `users_exams_questions` (`id`, `user_id`, `exam_id`, `question_id`, `user_answer`, `exam_score`) VALUES (4, 123456, 1, 4, "4::2", "80/100");
+INSERT INTO `users_exams_questions` (`id`, `user_id`, `exam_id`, `question_id`, `user_answer`, `exam_score`) VALUES (5, 123456, 1, 5, "2", "80/100");
+INSERT INTO `users_exams_questions` (`id`, `user_id`, `exam_id`, `question_id`, `user_answer`, `exam_score`) VALUES (6, 123456, 1, 6, "with", "80/100");
+INSERT INTO `users_exams_questions` (`id`, `user_id`, `exam_id`, `question_id`, `user_answer`, `exam_score`) VALUES (7, 123456, 1, 7, "4::3::2", "80/100");
+INSERT INTO `users_exams_questions` (`id`, `user_id`, `exam_id`, `question_id`, `user_answer`, `exam_score`) VALUES (8, 123456, 1, 8, "7", "80/100");
+INSERT INTO `users_exams_questions` (`id`, `user_id`, `exam_id`, `question_id`, `user_answer`, `exam_score`) VALUES (9, 123456, 1, 9, "meo", "80/100");
+INSERT INTO `users_exams_questions` (`id`, `user_id`, `exam_id`, `question_id`, `user_answer`, `exam_score`) VALUES (10, 123456, 1, 10, "7::8", "80/100");
 COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
--- server/server.js
+++ server/server.js
@@ -31,11 +31,33 @@ const FAKE_USER_ID = 123456
 
 app.post('/exams/:id', async (req, res) => {
   try {
-    // const listExams = await queryPromise(
+    const examData = await queryPromise(
+      mysqlConnector,
+      `SELECT * FROM users_exams_questions WHERE user_id=${FAKE_USER_ID} AND exam_id=${req.params.id} LIMIT 1`
+    )
+    if (examData.length) {
+      await queryPromise(
+        mysqlConnector,
+        `DELETE FROM users_exams_questions WHERE user_id=${FAKE_USER_ID} AND exam_id=${req.params.id}`
+      )
+    }
+    // TODO: cham diem
+    // const examQuestions = await queryPromise(
     //   mysqlConnector,
-    //   `SELECT * FROM exams INNER JOIN users_exams ON users_exams.user_id=${FAKE_USER_ID} AND users_exams.exam_id=exams.id`
+    //   `SELECT * FROM questions INNER JOIN exams ON exams.id=${req.params.id} INNER JOIN users_questions ON users_questions.question_id=questions.id`
     // )
-    res.json({ status: req.body })
+    // const parsedQuestionAnswers = Object.keys(req.body).map((questionId) => {
+    //   return `('${FAKE_USER_ID}', '${req.params.id}', '${questionId}', '${
+    //     req.body[questionId] || ''
+    //   }', '69/100')`
+    // })
+    // TODO: create a calculate-score function
+    await queryPromise(
+      mysqlConnector,
+      `INSERT INTO users_exams_questions (user_id, exam_id, question_id, user_answer, exam_score) VALUES ${parsedQuestionAnswers}`
+    )
+    console.log('result', result)
+    res.json({ status: 'result' })
   } catch (error) {
     res.json(error)
   }
@@ -47,14 +69,20 @@ app.get('/exams/:id', async (req, res) => {
       mysqlConnector,
       `SELECT * FROM exams WHERE exams.id=${req.params.id}`
     )
-    const examQuestions = await queryPromise(
+    const questions = await queryPromise(
       mysqlConnector,
-      `SELECT * FROM questions INNER JOIN exams ON exams.id=${req.params.id} INNER JOIN users_questions ON users_questions.question_id=questions.id`
+      `SELECT * FROM questions INNER JOIN exams_questions ON exams_questions.exam_id = ${req.params.id} AND exams_questions.question_id=questions.id`
     )
-    res.json({
-      ...(!!examInfo?.length ? examInfo?.[0] : {}),
-      questions: parseAnswerRightOrWrong(examQuestions),
-    })
+    // const examQuestions = await queryPromise(
+    //   mysqlConnector,
+    //   `SELECT DISTINCT questions.id, questions.title, questions.type, questions.option_1, questions.option_2, questions.option_3, questions.option_4, users_exams_questions.user_answer, users_exams_questions.exam_score FROM questions INNER JOIN users_exams_questions ON users_exams_questions.exam_id=${req.params.id}`
+    // )
+    console.log('questions', questions)
+    // res.json({
+    //   ...(!!examInfo?.length ? examInfo?.[0] : {}),
+    //   // questions: parseAnswerRightOrWrong(examQuestions),
+    // })
+    res.json(questions)
   } catch (error) {
     res.json(error)
   }
@@ -62,11 +90,16 @@ app.get('/exams/:id', async (req, res) => {
 
 app.get('/exams', async (req, res) => {
   try {
-    const listExams = await queryPromise(
+    const listExams = await queryPromise(mysqlConnector, `SELECT * FROM exams`)
+    const userExams = await queryPromise(
       mysqlConnector,
-      `SELECT * FROM exams INNER JOIN users_exams ON users_exams.user_id=${FAKE_USER_ID} AND users_exams.exam_id=exams.id`
+      `SELECT DISTINCT users_exams_questions.exam_id, users_exams_questions.user_id, users_exams_questions.exam_score FROM users_exams_questions WHERE users_exams_questions.user_id=${FAKE_USER_ID}`
     )
-    res.json(listExams)
+    const result = listExams?.map((exam) => ({
+      ...exam,
+      score: userExams?.find((e) => exam.id === e.exam_id)?.exam_score ?? null,
+    }))
+    res.json(result)
   } catch (error) {
     res.json(error)
   }
