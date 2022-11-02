2134e28aa54c351ec21170e808d1d25a4614dd9d
DinhThienPhuc <thienphuc.bka1412@gmail.com>
2022-11-03 01:15:39 +0700

    add auto script

---
+++ boot.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# Stop container
+docker compose down -v
+
+# Remove all images
+docker rmi -f $(docker images -aq)
+
+# Clean system
+docker system prune -y
+
+# Re-run server and database
+docker compose -f docker-compose-without-client.yml up -d
